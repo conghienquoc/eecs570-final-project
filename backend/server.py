727ef54e157e1a0cc6154ae01f05f63b10bc25e6
Nick Matton <mattonn@umich.edu>
2022-03-23 15:34:07 -0400

    backend file

---
+++ backend/server.py
@@ -0,0 +1,27 @@
+import flask
+from flask import make_response, jsonify
+
+app = flask.Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def show_index():
+    if flask.request.method == "POST":
+        request = flask.request.get_json(force=True)
+
+    output = {'test': 2}
+
+    return make_response(jsonify(**output))
+
+@app.route("/members")
+def members():
+    return {"members": ["Member1", "Member2", "Member3"]}
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# @app.route("/get_init_state")
+# def get_init_state():
+#     data = {}
+#     # how to return to "/"?
+#     # or do we make all different rquest types in "/"
+#     return redirect(url_for('success',name = user))
